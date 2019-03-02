960b21b4481713b4dab82e4ee36ee5431e1ca74b
Peter N Jenkin <peternjenkin@gmail.com>
2019-03-02 15:10:48 +0000

    challenge 10-131 - async/await contrast

--- currency-convert.js
+++ currency-convert.js
@@ -5,11 +5,11 @@
 // https://restcountries.eu/rest/v2/currency/{currency}
 
 
-/// get exchange rate *from* and *to* different currencies
 
 const axios = require('axios');
 
-// compare / contrast - non-async version
+/// get exchange rate *from* and *to* different currencies
+// compare / contrast - non-async/await version
 const getExchangeRate = (from, to) =>
 {
   return axios.get('http://data.fixer.io/api/latest?access_key=d1b90095d0b20832def727c348206e98')
@@ -22,6 +22,8 @@ const getExchangeRate = (from, to) =>
   });
 };
 
+// compare / contrast - non-async/await version
+/// get country info by use-of-currency-code
 const getCountries = (currencyCode) =>
 {
   return axios.get(`https://restcountries.eu/rest/v2/currency/${currencyCode}`).then((response) =>
@@ -30,8 +32,9 @@ const getCountries = (currencyCode) =>
   });
 };
 
-// compare / contrast - async version
-const getExchangeRateAwait = async (from, to) =>
+/// get exchange rate *from* and *to* different currencies
+// compare / contrast - async/await version
+const getExchangeRateAsync = async (from, to) =>
 {
   const response = await axios.get('http://data.fixer.io/api/latest?access_key=d1b90095d0b20832def727c348206e98');
   const euro = 1 / response.data.rates[from];
@@ -41,17 +44,30 @@ const getExchangeRateAwait = async (from, to) =>
 };
 
 
+// compare / contrast - async/await version
+/// get country info by use-of-currency-code
+const getCountriesAsync = async (currencyCode) =>
+{
+  const response = await axios.get(`https://restcountries.eu/rest/v2/currency/${currencyCode}`);
+  return response.data.map((country)=> country.name);     // ES6 arrow abbreviation
+};
+
+
 // --------------------------------------
 getExchangeRate('USD','CAD').then((rate) =>
 {
   console.log('non-async/await', rate);
 });
 
-getExchangeRateAwait('USD','CAD').then((rate) =>
+getExchangeRateAsync('USD','CAD').then((rate) =>
 {
   console.log('await', rate);
 });
 
 getCountries('EUR').then((countries) => {
+  console.log(countries);
+})
+
+getCountriesAsync('EUR').then((countries) => {
   console.log(countries);
 })
