c4575375fedcebdefbb9d241060decfae3ff671c
Peter N Jenkin <peternjenkin@gmail.com>
2019-03-02 12:30:55 +0000

    exampe code using multiple promises and thens setup (lengthy) - 10-129

--- app-promises.js
+++ app-promises.js
@@ -12,8 +12,25 @@ const users = [
   }
 ];
 
-const grades = [];
+const grades = [
+  {
+    id: 1,
+    schoolId: 101,
+    grade: 86,
+  },
+  {
+    id: 2,
+    schoolId: 999,
+    grade: 100,
+  },
+  {
+    id: 3,
+    schoolId: 101,
+    grade: 80,
+  }
+];
 
+/// search for a specific user by id
 const getUser = (id) =>
 {
   return new Promise((resolve, reject) =>
@@ -36,7 +53,55 @@ const getUser = (id) =>
   });
 };
 
-getUser(2).then((user)=>
+const getGrades = (schoolId) => {
+  return new Promise((resolve, reject) =>
+  {
+    resolve(grades.filter((grade) => grade.schoolId === schoolId))
+  });
+};
+
+// calculate for to return string '[name] has a [x]% average in the class'
+const getStatus = (userId) =>
+{
+  let user;     // just declare (undefined for now) with scope over any following promise/then
+  //  get the user (in promise), and then when ready (in promise) use the gotten user object's schoolId property to getGrades, then do calculations on same
+  return getUser(userId)
+  // .then((user) =>
+  .then((tempUser) =>
+  {
+    user = tempUser;      // assign user value to in-scope enclosing variable
+// console.log('user: ', user)
+    return getGrades(user.schoolId);
+  }).then((grades) =>
+  {
+    let average = 0;
+// console.log('got here');
+// console.log('grades: ', grades);
+
+    if (grades.length > 0)
+    {
+      average = grades.map((grade) => grade.grade).reduce((a, b) => a + b) / grades.length;
+      // array.reduce d'reduce array to a single value,
+      // executing a function on array members in turn left-to-right
+      // (e.g. for summing array contents (eg 1 field/property), before division by length for to average)
+// console.log('average: ', average);
+      return `${user.name} has a ${average}% in the class`;
+    }
+  });
+};
+
+getUser(2).then((user) =>
 {
   console.log(user);
+}).catch((error) => {console.log(error)});
+
+
+getGrades(999).then((grades) =>
+{
+  console.log(grades);
+}).catch((error) => {console.log(error)});
+
+getStatus(1).then((status) =>
+{
+  console.log(status);
 }).catch((error) => {console.log(error)});
